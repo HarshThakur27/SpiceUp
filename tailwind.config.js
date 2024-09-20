6901350f975c27c84490131c934cf7f891f649b8
Harsh Thakur <121555585+HarshThakur27@users.noreply.github.com>
2024-09-20 08:22:24 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: '#27355A',
+        custome: '#0F182C',
+      },
+      keyframes: {
+        clipReveal: {
+          '0%': { clipPath: 'inset(0 0 0 100%)', opacity: '0' },
+          '100%': { clipPath: 'inset(0 0 0 0)', opacity: '1' },
+        },
+        reveal: {
+          '0%': { clipPath: 'inset(100% 0 0 0)', transform: 'translateY(100%)' },
+          '100%': { clipPath: 'inset(0 0 0 0)', opacity: '1'  },
+        },
+      },
+      animation: {
+        clipReveal: 'clipReveal 2s ease forwards ',
+        reveal: 'reveal 3s ease forwards 4s',
+      },
+    },
   },
   plugins: [],
 }
