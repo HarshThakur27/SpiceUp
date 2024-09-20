a78a218a9d106f68a1206f3912d41d47f4114834
Harsh Thakur <121555585+HarshThakur27@users.noreply.github.com>
2024-09-20 08:41:23 +0530

    Update Search.jsx

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -89,7 +89,7 @@ const Search = () => {
   };
 
   return (
-    <div className="container bg-black w-full h-[600px] overflow-y-scroll mx-auto p-4  ">
+    <div className=" bg-black w-full h-[600px] overflow-y-scroll mx-auto  ">
      
       {/* <h1 className="text-3xl font-bold text-center mb-4">Search Recipes</h1> */}
       <div className="mb-8">
